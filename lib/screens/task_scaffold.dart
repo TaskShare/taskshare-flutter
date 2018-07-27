18304b2f7f6b66f1134dd32e90b99eff28dd8c69
Masayuki Ono <mono0926@gmail.com>
2018-07-27 13:34:27 +0900

    Refactor

--- lib/screens/task_scaffold.dart
+++ lib/screens/task_scaffold.dart
@@ -10,8 +10,6 @@ import 'package:taskshare/widgets/bottom_menu.dart';
 class TaskScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final defaultMediaQuery = MediaQuery.of(context);
-    log.info('mediaQuery: $defaultMediaQuery');
     final bloc = TasksProvider.of(context);
     return Scaffold(
       bottomNavigationBar: BottomMenu(),
@@ -19,55 +17,29 @@ class TaskScaffold extends StatelessWidget {
       body: TaskList(),
       floatingActionButton: AddTaskButton(
         onPressed: () async {
-          final value = await showDialog<String>(
-              context: context,
-              builder: (BuildContext context) {
-                log.info('mediaQuery: $defaultMediaQuery');
-                final viewInsets = MediaQuery.of(context).viewInsets;
-                final bottomInset =
-                    viewInsets.bottom - defaultMediaQuery.padding.bottom;
-                log.info('bottomInset: $bottomInset');
-                return AnimatedPadding(
-                  padding: EdgeInsets.only(
-                    top: viewInsets.top,
-                    left: viewInsets.left,
-                    right: viewInsets.right,
-                    bottom: math.max(0.0, bottomInset),
-                  ),
-                  duration: Duration(milliseconds: 100),
-                  curve: Curves.decelerate,
-                  child: new MediaQuery.removeViewInsets(
-                    removeLeft: true,
-                    removeTop: true,
-                    removeRight: true,
-                    removeBottom: true,
-                    context: context,
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Material(
-                        elevation: 24.0,
-                        type: MaterialType.card,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            TextField(
-                              autofocus: true,
-                            ),
-                            RaisedButton(
-                              child: Text('ADD'),
-                              onPressed: () async {
-                                bloc.add(new Task(id: null, title: 'aaa'));
-                                Navigator.of(context).pop('hoge');
-                              },
-                            ),
-                          ],
-                        ),
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Padding(
+                padding: MediaQuery.of(context).viewInsets,
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      TextField(
+                        autofocus: true,
                       ),
-                    ),
+                      RaisedButton(
+                        child: Text('ADD'),
+                        onPressed: () async {
+                          bloc.add(new Task(id: null, title: 'aaa'));
+                          Navigator.of(context).pop('hoge');
+                        },
+                      ),
+                    ],
                   ),
-                );
-              });
-          log.info(value);
+              );
+            },
+          );
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
