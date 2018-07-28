8aae6f41aa53f47a65f3fa798d5de0b63cd447ce
Masayuki Ono <mono0926@gmail.com>
2018-07-28 13:31:40 +0900

    Refactor

--- lib/widgets/add_task_button.dart
+++ lib/widgets/add_task_button.dart
@@ -4,6 +4,18 @@ import 'package:taskshare/export/export_ui.dart';
 class AddTaskButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Colors.transparent, // showModalBottomSheetの背景色をここだけ変えるためのWork Around
+      ),
+      child: _AddTaskButton(),
+    );
+  }
+}
+
+class _AddTaskButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return FloatingActionButton.extended(
       icon: Icon(Icons.add),
       label: Text('Add a new task'),
@@ -19,61 +31,58 @@ class AddTaskButton extends StatelessWidget {
       context: context,
       builder: (context) {
         return Container(
-          color: Colors.black54, // 仕方なくバリアビュー色合わせ
-          child: Container(
-            padding: MediaQuery.of(context).viewInsets,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
+          padding: MediaQuery.of(context).viewInsets,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 20.0,
+              right: 20.0,
+              top: 20.0,
+              bottom: 8.0,
             ),
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 20.0,
-                right: 20.0,
-                top: 20.0,
-                bottom: 8.0,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextField(
-                      autofocus: true,
-                      decoration: new InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'New Task',
-                      )),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                        padding: EdgeInsets.all(0.0),
-                        icon: Icon(
-                          Icons.open_in_new,
-                        ),
-                        onPressed: () {
-                          _pop(context);
-                        },
-                        color: Theme.of(context).accentColor,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                    autofocus: true,
+                    decoration: new InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'New Task',
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      padding: EdgeInsets.all(0.0),
+                      icon: Icon(
+                        Icons.open_in_new,
                       ),
-                      FlatButton(
-                        onPressed: () async {
-                          _saveTask(bloc, context);
-                        },
-                        child: Text(
-                          'SAVE',
-                          style: Theme.of(context).primaryTextTheme.button.apply(
-                                color: Theme.of(context).accentColor,
-                                fontWeightDelta: 2,
-                              ),
-                        ),
-                        color: Colors.white,
-                        splashColor: Colors.blue,
-                        textColor: Theme.of(context).accentColor,
+                      onPressed: () {
+                        _pop(context);
+                      },
+                      color: Theme.of(context).accentColor,
+                    ),
+                    FlatButton(
+                      onPressed: () async {
+                        _saveTask(bloc, context);
+                      },
+                      child: Text(
+                        'SAVE',
+                        style: Theme.of(context).primaryTextTheme.button.apply(
+                              color: Theme.of(context).accentColor,
+                              fontWeightDelta: 2,
+                            ),
                       ),
-                    ],
-                  ),
-                ],
-              ),
+                      color: Colors.white,
+                      splashColor: Colors.blue,
+                      textColor: Theme.of(context).accentColor,
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         );
