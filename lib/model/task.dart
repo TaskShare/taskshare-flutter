d241f2776084c6b547acb95ce522bf8a15891e7e
Masayuki Ono <mono0926@gmail.com>
2018-07-31 19:31:17 +0900

    Override toString of Task

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -33,30 +33,42 @@ class Task implements Entity {
 
   @override
   int get hashCode => id.hashCode ^ title.hashCode;
+
+  @override
+  String toString() => '''
+
+Task{
+  id: $id, 
+  title: $title, 
+  doneTime: $doneTime, 
+  dueTime: $dueTime, 
+  createTime: $createTime, 
+  updateTime: $updateTime
+}''';
 }
 
 class TaskEncoder extends SnapshotEncoder<Task> {
   @override
   Map<String, dynamic> encode(Task entity) => {
-      Entity.idKey: entity.id,
-      TaskProperties.title: entity.title,
-      TaskProperties.doneTime: entity.doneTime,
-      TaskProperties.dueTime: entity.dueTime,
-      TaskProperties.createTime: entity.createTime,
-      TaskProperties.updateTime: entity.updateTime
-    };
+        Entity.idKey: entity.id,
+        TaskProperties.title: entity.title,
+        TaskProperties.doneTime: entity.doneTime,
+        TaskProperties.dueTime: entity.dueTime,
+        TaskProperties.createTime: entity.createTime,
+        TaskProperties.updateTime: entity.updateTime
+      };
 }
 
 class TaskDecoder extends SnapshotDecoder<Task> {
   @override
   Task decode(Map<String, dynamic> data) => Task(
-      id: data[Entity.idKey],
-      title: data[TaskProperties.title],
-      doneTime: data[TaskProperties.doneTime],
-      dueTime: data[TaskProperties.dueTime],
-      createTime: data[TaskProperties.createTime],
-      updateTime: data[TaskProperties.updateTime],
-    );
+        id: data[Entity.idKey],
+        title: data[TaskProperties.title],
+        doneTime: data[TaskProperties.doneTime],
+        dueTime: data[TaskProperties.dueTime],
+        createTime: data[TaskProperties.createTime],
+        updateTime: data[TaskProperties.updateTime],
+      );
 }
 
 // 'dart:mirrors'がDart 2では未実装なので
