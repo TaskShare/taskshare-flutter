c440fd89aca8f3e2b1b2052f3335b3e895e047aa
Masayuki Ono <mono0926@gmail.com>
2018-08-02 15:14:32 +0900

    Add FirApp configuration sample

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,21 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+@import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // TODO: Configurationによって参照するファイル名を切り替え
+//    NSString* path = [[NSBundle mainBundle] pathForResource: @"GoogleService-Info2" ofType: @"plist"];
+//    FIROptions* options = [[FIROptions alloc] initWithContentsOfFile: path];
+//    [FIRApp configureWithOptions: options];
+//    NSLog(@"projectID: %@", FIRApp.defaultApp.options.projectID);
+
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    // Override point for customization after application launch.
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
+
