39ce7ad4f1c3f5166ce4322d37ee71c0f4030a8b
Masayuki Ono <mono0926@gmail.com>
2018-09-02 18:32:47 +0900

    Configure firebase app by config

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,12 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // TODO: Configurationによって参照するファイル名を切り替え
+//    let path = Bundle.main.path(forResource: "GoogleService-Info2", ofType: "plist")!
+//    FirebaseApp.configure(options: FirebaseOptions(contentsOfFile: path)!)
+//    print("projectID: \(FirebaseApp.app()?.options.projectID ?? "")");
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
